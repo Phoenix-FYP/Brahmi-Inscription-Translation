6f0291cf46584b584702165a251a1aad92cfc3c8
Nadunrupasinghe <nadunmalindarupasinghe@gmail.com>
2025-07-26 23:51:34 +0530

    Installed tailwindcss and configured it in Vite project.

--- frontend/src/screens/module2.jsx
+++ frontend/src/screens/module2.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Module2 = () => {
   return (
     <div className="">
-      <h2 className="">Module 2</h2>
+      <h2 className="h-5 text-blue-500">Module 2</h2>
       <p>This is the content of Module 2.</p>
     </div>
   );
